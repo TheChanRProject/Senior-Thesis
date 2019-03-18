0eaedf65eafab5a512f4bbaca95aa9003a1db4dc
Rishov Chatterjee <garpchoo@gmail.com>
2019-03-17 17:40:05 -0700

    Editing

--- src/audio_processing/mp3_downsample.py
+++ src/audio_processing/mp3_downsample.py
@@ -3,13 +3,13 @@ from pydub import AudioSegment
 
 cwd = os.getcwd()
 print(cwd)
-file_path = '/Users/rchatter/Desktop/DS/Python/Senior-Thesis/music_files/originals'
-file_name="/pandit-hari-orig.mp3"
+file_path = '/Users/rchatter/Desktop/DS/Python/Senior-Thesis/music_files/originals/'
+file_name="pandit-hari-todi-orig.mp3"
 print(file_name[:-8])
 out_path = '/Users/rchatter/Desktop/DS/Python/Senior-Thesis/music_files/samples'
 
 startMin = 1
-startSec = 30
+startSec = 00
 
 endMin = 2
 endSec = 00
@@ -24,4 +24,4 @@ song = AudioSegment.from_mp3(file_path+file_name)
 extract = song[startTime:endTime]
 
 # Save
-extract.export(out_path+file_name[:-4]+'extract.mp3', format='mp3')
+extract.export(out_path+file_name[:-8]+'extract.mp3', format='mp3')
