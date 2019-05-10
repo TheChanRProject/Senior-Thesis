a2407dd40cbcde0fd1a091e14abc0018f2a5669d
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-10 02:07:10 -0700

    Adding edits

--- src/audio_processing/wav-convert.py
+++ src/audio_processing/wav-convert.py
@@ -1,6 +1,11 @@
 import numpy as np
 import soundfile as sf
 from sksound.sounds import Sound
+import os
 
-mySound = Sound("data/Hindustani/mp3/Des/des1.mp3")
-mySound.write_wav("data/Hindustani/wav/Des/des1.wav")
+desFiles = os.listdir("data/Hindustani/mp3/Des")
+
+
+for file in desFiles:
+    mySound = Sound(file)
+    mySound.write_wav()
