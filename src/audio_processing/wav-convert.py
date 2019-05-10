78afa0d9ceea224662b465bf8ae10398e7bd0510
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-10 02:50:32 -0700

    Adding edits

--- src/audio_processing/wav-convert.py
+++ src/audio_processing/wav-convert.py
@@ -4,32 +4,29 @@ from sksound.sounds import Sound
 import os
 
 mySound = Sound("data/Hindustani/mp3/Yaman/yaman1.mp3")
-mySound.write_wav()
 
 
 mySound = Sound("data/Hindustani/mp3/Yaman/yaman2.mp3")
-mySound.write_wav()
+
 
 mySound = Sound("data/Hindustani/mp3/Yaman/yaman3.mp3")
-mySound.write_wav()
+
 
 mySound = Sound("data/Hindustani/mp3/Yaman/yaman4.mp3")
-mySound.write_wav()
+
 
 mySound = Sound("data/Hindustani/mp3/Yaman/yaman5.mp3")
-mySound.write_wav()
+
 
 mySound = Sound("data/Hindustani/mp3/Yaman/yaman6.mp3")
-mySound.write_wav()
+
 
 mySound = Sound("data/Hindustani/mp3/Yaman/yaman7.mp3")
-mySound.write_wav()
+
 
 mySound = Sound("data/Hindustani/mp3/Yaman/yaman8.mp3")
-mySound.write_wav()
 
 mySound = Sound("data/Hindustani/mp3/Yaman/yaman9.mp3")
-mySound.write_wav()
+
 
 mySound = Sound("data/Hindustani/mp3/Yaman/yaman10.mp3")
-mySound.write_wav()
