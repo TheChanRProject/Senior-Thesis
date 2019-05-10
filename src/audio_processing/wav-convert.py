502d53d66490ca2c2b0578f1c260bda7c5d70e09
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-10 02:15:45 -0700

    Adding edits

--- src/audio_processing/wav-convert.py
+++ src/audio_processing/wav-convert.py
@@ -6,6 +6,33 @@ import os
 desFiles = os.listdir("data/Hindustani/mp3/Des")
 
 
-for file in desFiles:
-    mySound = Sound(file)
-    mySound.write_wav()
+mySound = Sound(desFiles[0])
+mySound.write_wav("data/Hindustani/wav/Des/des1.wav")
+
+
+mySound = Sound(desFiles[1])
+mySound.write_wav("data/Hindustani/wav/Des/des2.wav")
+
+mySound = Sound(desFiles[2])
+mySound.write_wav("data/Hindustani/wav/Des/des3.wav")
+
+mySound = Sound(desFiles[3])
+mySound.write_wav("data/Hindustani/wav/Des/des4.wav")
+
+mySound = Sound(desFiles[4])
+mySound.write_wav("data/Hindustani/wav/Des/des5.wav")
+
+mySound = Sound(desFiles[5])
+mySound.write_wav("data/Hindustani/wav/Des/des6.wav")
+
+mySound = Sound(desFiles[6])
+mySound.write_wav("data/Hindustani/wav/Des/des7.wav")
+
+mySound = Sound(desFiles[7])
+mySound.write_wav("data/Hindustani/wav/Des/des8.wav")
+
+mySound = Sound(desFiles[8])
+mySound.write_wav("data/Hindustani/wav/Des/des9.wav")
+
+mySound = Sound(desFiles[9])
+mySound.write_wav("data/Hindustani/wav/Des/des10.wav")
