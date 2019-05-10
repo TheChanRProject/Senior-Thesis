c541815e63697df06aa66346c80f347b9bbd4a3c
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-10 02:48:29 -0700

    Adding edits

--- src/audio_processing/wav-convert.py
+++ src/audio_processing/wav-convert.py
@@ -3,33 +3,33 @@ import soundfile as sf
 from sksound.sounds import Sound
 import os
 
-mySound = Sound("data/Hindustani/mp3/Des/des1.mp3")
-mySound.write_wav("data/Hindustani/wav/Des/des1.wav")
+mySound = Sound("data/Hindustani/mp3/Yaman/yaman1.mp3")
+mySound.write_wav()
 
 
-mySound = Sound("data/Hindustani/mp3/Des/des2.mp3")
-mySound.write_wav("data/Hindustani/wav/Des/des2.wav")
+mySound = Sound("data/Hindustani/mp3/Yaman/yaman2.mp3")
+mySound.write_wav()
 
-mySound = Sound("data/Hindustani/mp3/Des/des3.mp3")
-mySound.write_wav("data/Hindustani/wav/Des/des3.wav")
+mySound = Sound("data/Hindustani/mp3/Yaman/yaman3.mp3")
+mySound.write_wav()
 
-mySound = Sound("data/Hindustani/mp3/Des/des4.mp3")
-mySound.write_wav("data/Hindustani/wav/Des/des4.wav")
+mySound = Sound("data/Hindustani/mp3/Yaman/yaman4.mp3")
+mySound.write_wav()
 
-mySound = Sound("data/Hindustani/mp3/Des/des5.mp3")
-mySound.write_wav("data/Hindustani/wav/Des/des5.wav")
+mySound = Sound("data/Hindustani/mp3/Yaman/yaman5.mp3")
+mySound.write_wav()
 
-mySound = Sound("data/Hindustani/mp3/Des/des6.mp3")
-mySound.write_wav("data/Hindustani/wav/Des/des6.wav")
+mySound = Sound("data/Hindustani/mp3/Yaman/yaman6.mp3")
+mySound.write_wav()
 
-mySound = Sound("data/Hindustani/mp3/Des/des7.mp3")
-mySound.write_wav("data/Hindustani/wav/Des/des7.wav")
+mySound = Sound("data/Hindustani/mp3/Yaman/yaman7.mp3")
+mySound.write_wav()
 
-mySound = Sound("data/Hindustani/mp3/Des/des8.mp3")
-mySound.write_wav("data/Hindustani/wav/Des/des8.wav")
+mySound = Sound("data/Hindustani/mp3/Yaman/yaman8.mp3")
+mySound.write_wav()
 
-mySound = Sound("data/Hindustani/mp3/Des/des9.mp3")
-mySound.write_wav("data/Hindustani/wav/Des/des9.wav")
+mySound = Sound("data/Hindustani/mp3/Yaman/yaman9.mp3")
+mySound.write_wav()
 
-mySound = Sound("data/Hindustani/mp3/Des/des10.mp3")
-mySound.write_wav("data/Hindustani/wav/Des/des10.wav")
+mySound = Sound("data/Hindustani/mp3/Yaman/yaman10.mp3")
+mySound.write_wav()
