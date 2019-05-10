40a2778c702aa76923988c01d1f854cbfe194b72
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-10 05:52:03 -0700

    Adding edits

--- src/audio_processing/wav-convert.py
+++ src/audio_processing/wav-convert.py
@@ -3,25 +3,29 @@ import soundfile as sf
 from sksound.sounds import Sound
 import os
 
-mySound = Sound("data/Hindustani/mp3/Bhup/bhup1.mp3")
+mySound = Sound("data/Hindustani/mp3/Bhairav/bhairav1.mp3")
 
 
-mySound = Sound("data/Hindustani/mp3/Bhup/bhup2.mp3")
+mySound = Sound("data/Hindustani/mp3/Bhairav/bhairav2.mp3")
 
 
-mySound = Sound("data/Hindustani/mp3/Bhup/bhup3.mp3")
+mySound = Sound("data/Hindustani/mp3/Bhairav/bhairav3.mp3")
 
 
-mySound = Sound("data/Hindustani/mp3/Bhup/bhup4.mp3")
+mySound = Sound("data/Hindustani/mp3/Bhairav/bhairav4.mp3")
 
 
-mySound = Sound("data/Hindustani/mp3/Bhup/bhup5.mp3")
+mySound = Sound("data/Hindustani/mp3/Bhairav/bhairav5.mp3")
 
 
-mySound = Sound("data/Hindustani/mp3/Bhup/bhup6.mp3")
+mySound = Sound("data/Hindustani/mp3/Bhairav/bhairav6.mp3")
 
 
-mySound = Sound("data/Hindustani/mp3/Bhup/bhup7.mp3")
+mySound = Sound("data/Hindustani/mp3/Bhairav/bhairav7.mp3")
 
 
-mySound = Sound("data/Hindustani/mp3/Bhup/bhup8.mp3")
+mySound = Sound("data/Hindustani/mp3/Bhairav/bhairav8.mp3")
+
+mySound = Sound("data/Hindustani/mp3/Bhairav/bhairav9.mp3")
+
+mySound = Sound("data/Hindustani/mp3/Bhairav/bhairav10.mp3")
