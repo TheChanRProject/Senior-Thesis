b4385c4ff32ad8e58d619e3e24a27701d8a8bdbe
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-10 21:14:01 -0700

    Adding edits

--- src/chromagrams/chroma_stft_gen.py
+++ src/chromagrams/chroma_stft_gen.py
@@ -134,3 +134,21 @@ for j, chroma in enumerate(chromas):
     specshow(chroma, x_axis="time", y_axis="chroma", vmin=0, vmax=1)
     plt.title(f"Chromagram of Bhairav7_{j}")
     plt.savefig(f"data/chroma_files/bhairav-chromas/bhairav7/bhairav7_{j}.png")
+
+## Bhairav 8
+
+block_gen = sf.blocks('data/Hindustani/wav/Bhairav/bhairav8.wav', blocksize=2646000)
+rate = sf.info("data/Hindustani/wav/Bhairav/bhairav8.wav").samplerate
+info = sf.info("data/Hindustani/wav/Bhairav/bhairav8.wav")
+print(info)
+chromas = []
+
+for bl in block_gen:
+    y = np.mean(bl, axis=1)
+    chromas.append(chroma_stft(y, sr=rate))
+
+len(chromas)
+for j, chroma in enumerate(chromas):
+    specshow(chroma, x_axis="time", y_axis="chroma", vmin=0, vmax=1)
+    plt.title(f"Chromagram of Bhairav8_{j}")
+    plt.savefig(f"data/chroma_files/bhairav-chromas/bhairav8/bhairav8_{j}.png")
