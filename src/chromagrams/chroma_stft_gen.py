47f7e67f49a441dca07ce5ebc3f4eebe2facc6ae
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-10 19:37:45 -0700

    Adding edits

--- src/chromagrams/chroma_stft_gen.py
+++ src/chromagrams/chroma_stft_gen.py
@@ -2,3 +2,19 @@ import numpy as np
 !pip install pysoundfile
 import soundfile as sf
 from librosa.feature import chroma_stft
+
+# Bhairav Block Wise Reading
+
+## Bhairav 1
+
+block_gen = sf.blocks('data/Hindustani/wav/Bhairav/bhairav1.wav', blocksize=1024, overlap=512)
+rate = sf.info("data/Hindustani/wav/Bhairav/bhairav1.wav").samplerate
+
+chromas = []
+
+for bl in block_gen:
+    y = np.mean(bl, axis=1)
+    chromas.append(chroma_stft(y, sr=rate))
+
+len(chromas)
+print(chromas[1])
