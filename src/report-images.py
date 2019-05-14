127bc1f595f16f83b80e6fcc1c257fa6b5e69bd0
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-13 22:51:42 -0700

    Adding edits

--- src/report-images.py
+++ src/report-images.py
@@ -1,3 +1,19 @@
 import os
-from librosa import load, stft
+import numpy as np
+from librosa import load, stft, amplitude_to_db
 from librosa.feature import melspectrogram, chroma_cqt, tonnetz
+from librosa.display import specshow
+import matplotlib.pyplot as plt
+
+sampFile = os.listdir("data/Hindustani/mp3/Bhairav")
+
+my_file = f"data/Hindustani/mp3/Bhairav/{sampFile[0]}"
+
+y, sr = load(my_file)
+
+D = np.abs(stft(y))
+
+specshow(amplitude_to_db(D, ref=np.max), y_axis='log', x_axis='time')
+plt.title("Power Spectrogram")
+plt.colorbar(format='%+2.0f dB')
+plt.tight_layout()
