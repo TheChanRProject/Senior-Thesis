0b83d4d8becaddb657944a56acca0591dff6046e
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-13 23:21:14 -0700

    Adding edits

--- src/report-images.py
+++ src/report-images.py
@@ -17,3 +17,10 @@ specshow(amplitude_to_db(D, ref=np.max), y_axis='log', x_axis='time')
 plt.title("Power Spectrogram")
 plt.colorbar(format='%+2.0f dB')
 plt.tight_layout()
+
+chroma_cq = chroma_cqt(y=y,sr=sr)
+specshow(chroma_cq, y_axis='chroma', x_axis='time')
+plt.title("Chromagram Constant Q Transform")
+plt.colorbar()
+plt.tight_layout()
+    
