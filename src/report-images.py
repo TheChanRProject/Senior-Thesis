e2f84979fa7a6549f8617f0fcb951fa3fbd72049
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-14 00:02:16 -0700

    Adding edits

--- src/report-images.py
+++ src/report-images.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from librosa import load, stft, amplitude_to_db
+from librosa import load, stft, amplitude_to_db, power_to_db
 from librosa.feature import melspectrogram, chroma_cqt, tonnetz
 from librosa.display import specshow
 import matplotlib.pyplot as plt
@@ -28,4 +28,10 @@ tonnetz = tonnetz(y=y, sr=sr)
 specshow(tonnetz, y_axis='tonnetz')
 plt.title("Tonnetz Example")
 plt.colorbar()
+plt.tight_layout()
+
+ms = melspectrogram(y=y, sr=sr)
+specshow(power_to_db(ms, ref=np.max), y_axis='mel', fmax=8000, x_axis='time')
+plt.title("Mel Spectrogram Example")
+plt.colorbar(format="%+2.0f dB")
 plt.tight_layout()
